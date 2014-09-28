91730be47832840b0b1442717035bce5b94f30b4
paperray <paperraytanute@gmail.com>
2014-09-28 15:52:06 -0700

    remove db

--- src/Paperstreetmedia/DataAccessObject/LaravelMysqlObject.php
+++ src/Paperstreetmedia/DataAccessObject/LaravelMysqlObject.php
@@ -1,8 +1,8 @@
 <?php
 namespace Paperstreetmedia\DataAccessObject;
 
-use Illuminate\Database\Capsule\Manager as DB;
-
+//use Illuminate\Database\Capsule\Manager as DB;
+use \DB;
 class LaravelMysqlObject extends GirlObjectBaseClass implements GirlObjectInterface
 {
 	public function create()
