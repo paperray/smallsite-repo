dd0832b31a9dcff508e67b38e6e1fa3ad497a59c
paperray <paperraytanute@gmail.com>
2014-09-28 15:59:17 -0700

    addded capsule init

--- src/Paperstreetmedia/DataAccessObject/LaravelMysqlObject.php
+++ src/Paperstreetmedia/DataAccessObject/LaravelMysqlObject.php
@@ -5,6 +5,35 @@ use Illuminate\Database\Capsule\Manager as DB;
 
 class LaravelMysqlObject extends GirlObjectBaseClass implements GirlObjectInterface
 {
+	public function __construct()
+	{
+		$capsule = new DB;
+
+		$capsule->addConnection([
+			'driver'    => 'mysql',
+			'host'      => 'localhost',
+			'database'  => 'laraveltubetour',
+			'username'  => 'root',
+			'password'  => 'secret',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+		]);
+
+		// Set the event dispatcher used by Eloquent models... (optional)
+		use Illuminate\Events\Dispatcher;
+		use Illuminate\Container\Container;
+		$capsule->setEventDispatcher(new Dispatcher(new Container));
+
+		// Make this Capsule instance available globally via static methods... (optional)
+		$capsule->setAsGlobal();
+
+		//Set to fecth class in order to return an object
+		$capsule->setFetchMode(PDO::FETCH_CLASS);
+
+		// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+		$capsule->bootEloquent();
+	}
 	public function create()
 	{
 	
